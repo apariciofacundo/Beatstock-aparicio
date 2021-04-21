fa835cc89572be3d2aee66ec7a964216f486126d
Facundo Diego Aparicio <Aparicio@aparicio.local>
2021-04-21 19:12:25 -0300

    desafio 3

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -3,10 +3,7 @@ import './itemListContainer.css'
 export function ItemListContainer() {
     return(
         <article>
-            <div 
-            className='background'>        
-            </div>
-            <h2 className='itemListContainer'>Discover and link with millions of creators</h2>
+            <h2 className='itemListContainer'>Discover and link with millions of creators</h2> 
         </article>
 
     )
